b4493a922447b0d0e5e32dae887dcf64f617fe70
Jitendra Rewar <jiturewar9@gmail.com>
2026-04-10 22:22:12 +0530

    add user

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,18 +1,9 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
+  name: { type: String, required: true },
+  email: { type: String, unique: true },
+  password: { type: String, required: true },
   role: {
     type: String,
     enum: ["admin", "member"],
